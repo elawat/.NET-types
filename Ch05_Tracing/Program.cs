a4ad9442b7fd51d8edf024086d789a5f091fef19
elzbietawatroba <elzbietawatroba@UKLOW71485>
2016-11-21 18:13:49 +0000

    Debugging

---
+++ Ch05_Tracing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+
+
+namespace Ch05_Tracing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Debug says Hello!");
+            Trace.WriteLine("Trace says Hello!");
+            WriteLine("Press ENTER to close.");
+            ReadLine();
+        }
+    }
+}
