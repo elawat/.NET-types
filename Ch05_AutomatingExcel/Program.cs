e3ec9d79f434adf02b46a51f232c200f031a393d
elzbietawatroba <elzbietawatroba@UKLOW71485>
2016-11-22 17:03:16 +0000

    Internalization and automating excel

---
+++ Ch05_AutomatingExcel/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using static System.Convert;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace Ch05_AutomatingExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int xlPie = 5;
+            Write("Enter a number ");
+            double number = ToDouble(ReadLine());
+            var excel = new Application();
+            excel.Visible = true;
+            excel.Workbooks.Add();
+            excel.Range["A1"].Value = number;
+            excel.Range["A2"].Formula = "=A1*2";
+            excel.Range["A1:A1"].Select();
+            excel.Charts.Add();
+            excel.ActiveChart.SetSourceData(Source: excel.Range["Sheet1!$A$1:$A$2"]);
+        }
+    }
+}
